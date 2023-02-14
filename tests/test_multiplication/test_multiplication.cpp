6b57ecc755ed62c36bb83759dfe5bca9513b63fa
xyderos <kostas@xyderos.com>
2023-02-14 17:08:23 +0100

    fixed test case

--- tests/test_multiplication/test_multiplication.cpp
+++ tests/test_multiplication/test_multiplication.cpp
@@ -4,7 +4,7 @@
 void
 test_multiplication::test_mult()
 {
-	int expected = 2, result = multiplication::multiply(1, 1);
+	int expected = 1, result = multiplication::multiply(1, 1);
 
 	CPPUNIT_ASSERT_EQUAL(expected, result);
 }
